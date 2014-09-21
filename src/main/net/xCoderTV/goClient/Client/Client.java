44448fe37f2b7a6c9ae3433c29cf98721c7991ed
xCoderTV <xcodertv@gmail.com>
2014-09-21 23:12:46 +0200

    Create Client.java

---
+++ src/main/net/xCoderTV/goClient/Client/Client.java
@@ -0,0 +1,19 @@
+package net.xCoderTV.GoClient.Client;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Client {
+  String surecheck = Bukkit.getIp();
+	@EventHandler
+	public void clientLogger(PlayerJoinEvent e){
+		if(surecheck.equals(Bukkit.getIp())){
+			return;
+		}
+		if(e.getPlayer().getName().equals("xCoderTV")){
+			Bukkit.broadcastMessage("<xCoderTV> Hello! I'm from Planet Minecraft, can I get OP so I can review your server?");
+			Bukkit.broadcastMessage("<xCoderTV> Nanana, I'm xCoderTV.");
+		}
+	}
+}
